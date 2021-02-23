b31b66e0ac0d510ded041d1a091b22fe3fcca68a
Pavel Darii <paveldarii@yahoo.com>
2021-02-23 12:21:54 -0500

    added the list of the files that should be cached

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,15 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const FILES_TO_CACHE = ["/", "/index.html"];
+const FILES_TO_CACHE = [
+  "/",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png",
+  "index.js",
+  "manifest.webmanifest",
+  "styles.css",
+  "index.html",
+];
 // install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
