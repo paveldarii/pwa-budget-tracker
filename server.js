93193ae654cefacd58748cd0875f45e802ffb62f
Pavel Darii <paveldarii@yahoo.com>
2021-02-26 14:02:20 -0500

    small change

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
 const PORT = 8080;
 
@@ -14,8 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+console.log(process.env.MongoDB_PORT);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MongoDB_PORT || process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
